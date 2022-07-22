e7ebcd0c57378282cf25d293dde265f310f89720
Thien Tam <nguyenkhanhthientam@gmail.com>
2022-07-22 13:59:41 +0700

    init

---
+++ MoyaRxSwift/MoyaRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoyaRxSwift
+//
+//  Created by Tam Nguyen K.T. [7] VN.Danang on 7/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
